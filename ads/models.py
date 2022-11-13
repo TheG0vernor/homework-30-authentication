629038c9dd37c841c0d2ae9a9f69f4e31ae1f932
Vadim <niras.sp@gmail.com>
2022-11-13 15:22:56 +0700

    release

---
+++ ads/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from users.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=60)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    price = models.PositiveIntegerField()
+    description = models.TextField(max_length=1000, null=True)
+    is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
